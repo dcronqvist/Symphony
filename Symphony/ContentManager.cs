6d5de3aca4a4e3decd32d17b94d891ae9c22af5d
Daniel Cronqvist <dancro99@gmail.com>
2022-08-26 02:44:16 +0200

    ContentManager can now load asynchronously

--- Symphony/ContentManager.cs
+++ Symphony/ContentManager.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 
 namespace Symphony;
 
@@ -94,6 +95,11 @@ public class ContentManager<TMeta> where TMeta : ContentMetadata
                 }
             }
         }
+    }
+
+    public async Task LoadAsync()
+    {
+        await Task.Run(() => this.Load());
     }
 
     public void Load()
