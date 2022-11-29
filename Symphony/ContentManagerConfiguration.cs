1f3cb4a0c950877822863d3e7740d92cde945990
Daniel Cronqvist <dancro99@gmail.com>
2022-11-29 21:55:02 +0100

    Configuration properties readable from other namespaces

--- Symphony/ContentManagerConfiguration.cs
+++ Symphony/ContentManagerConfiguration.cs
@@ -2,10 +2,10 @@ namespace Symphony;
 
 public class ContentManagerConfiguration<TMeta> where TMeta : ContentMetadata
 {
-    internal IContentStructureValidator<TMeta> StructureValidator { get; private set; }
-    internal IContentCollectionProvider CollectionProvider { get; private set; }
-    internal IContentLoader<TMeta> Loader { get; private set; }
-    internal bool HotReload { get; private set; }
+    public IContentStructureValidator<TMeta> StructureValidator { get; private set; }
+    public IContentCollectionProvider CollectionProvider { get; private set; }
+    public IContentLoader<TMeta> Loader { get; private set; }
+    public bool HotReload { get; private set; }
 
     public ContentManagerConfiguration(IContentStructureValidator<TMeta> validator, IContentCollectionProvider collectionProvider, IContentLoader<TMeta> loader, bool hotReload = false)
     {
